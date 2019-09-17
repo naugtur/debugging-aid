5be9911ef44f3477e80ca1cadb1b578bf495951c
Naugtur <naugtur@gmail.com>
2019-09-17 21:51:49 +0200

    promisecount

---
+++ promisecount.js
@@ -0,0 +1,16 @@
+const { printMessage } = require('./lib/print')
+
+let count = 0;
+const asyncHooks = require('async_hooks')
+const asyncHook = asyncHooks.createHook({
+    init(asyncId, type, triggerAsyncId) {
+        if (type === 'PROMISE') {
+            count++
+        }
+    },
+})
+asyncHook.enable()
+
+process.on('beforeExit', () => {
+    printMessage(`promises created: ${count}`)
+});
--- test/manual-hooks.js
+++ test/manual-hooks.js
@@ -1,5 +1,6 @@
 //AID_HOOK_SCOPE='test/cases/promise' node --trace-event-categories node.perf test/manual-hooks.js 
 
 require('../hooks')
+require('../promisecount')
 
 require('./cases/promise')()

29c15b612a04b2c30cf0649e480b3fc346bc6c00
Zbyszek Tenerowicz <naugtur@gmail.com>
2023-07-28 11:34:22 +0200

    Update curl.js to support https :facepalm:

--- curl.js
+++ curl.js
@@ -1,9 +1,17 @@
 const http = require('http')
+const https = require('https')
 require('request-to-curl')
 const { printMessage } = require('./lib/print')
 const oldRequest = http.request
 http.request = (options, callback) => {
   const re = oldRequest(options, callback)
+  re.on('finish', () => printMessage(re.toCurl()))
+
+  return re
+}
+const oldRequestS = https.request
+https.request = (options, callback) => {
+  const re = oldRequestS(options, callback)
   re.on('finish', () => printMessage(re.toCurl()))
 
   return re
